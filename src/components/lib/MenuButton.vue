c7adc39f9045eb6de53e54b158fd3dd586c9bf4e
Yao Kaiser <yao@ywk.ch>
2021-10-04 16:42:51 +0200

    fix: mobile click

--- src/components/lib/MenuButton.vue
+++ src/components/lib/MenuButton.vue
@@ -6,6 +6,8 @@
         ><button
           class="inline-flex justify-center w-full px-4 py-2 text-sm font-medium leading-5 text-white transition duration-150 ease-in-out bg-gray-800 border border-gray-600 rounded-md hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800"
           type="button"
+          @click="open = !open"
+
           aria-haspopup="true"
           aria-expanded="true"
           aria-controls="headlessui-menu-items-117"
