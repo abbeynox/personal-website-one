e204eeb22062a9611cdf0779e22ed6d6d6837d7f
Yao Kaiser <yao@ywk.ch>
2021-10-06 22:53:04 +0200

    update: skills cards centred

--- src/components/cards/SkillCard.vue
+++ src/components/cards/SkillCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-full p-2 px-2 my-2 overflow-hidden border border-gray-800 rounded-lg sm:my-2 sm:px-2 sm:w-full md:my-2 md:px-2 md:w-1/2 lg:my-2 lg:px-2 lg:w-1/3 xl:my-2 xl:px-2 xl:w-1/3 hover:bg-indigo-500 hover:bg-opacity-10 hover:shadow-lg hover:border-indigo-500"> <!-- flex entfernen damit zentriert -->
+    <div class="w-full p-2 px-2 my-2 overflow-hidden border border-gray-800 rounded-lg hover:bg-indigo-500 hover:bg-opacity-10 hover:shadow-lg hover:border-indigo-500"> <!-- flex entfernen damit zentriert -->
         <h2 class="font-medium text-white title-font"><i :class="icon"></i>  {{ name }} </h2>
     </div>
 </template> 
