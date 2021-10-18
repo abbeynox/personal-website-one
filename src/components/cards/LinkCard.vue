940f165fd011c84940b87390606f42ac7147b324
Yao Kaiser <yao@ywk.ch>
2021-10-18 22:23:35 +0200

    test: other colors for links (1)

--- src/components/cards/LinkCard.vue
+++ src/components/cards/LinkCard.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="w-full p-1 px-0 mx-6">
         <a :href="url" target="_blank">
-            <div :class="'flex items-center px-3 py-4 border border-gray-800 rounded-lg bg-gradient-to-r from-' + fromColor + ' ' + viaColor + ' ' +' to-'+ toColor + ' hover:bg-indigo-500 hover:bg-opacity-10 hover:shadow-lg hover:border-indigo-500'">
+            <!--<div :class="'flex items-center px-3 py-4 border border-gray-800 rounded-lg bg-gradient-to-r from-' + fromColor + ' ' + viaColor + ' ' +' to-'+ toColor + ' hover:bg-indigo-500 hover:bg-opacity-10 hover:shadow-lg hover:border-indigo-500'">-->
+            <div class="'flex items-center px-3 py-4 border border-gray-800 rounded-lg bg-gradient-to-r from-purple-400 to-red-500 hover:bg-indigo-500 hover:bg-opacity-10 hover:shadow-lg hover:border-indigo-500'">
                 <div class="flex-grow ">
                     <h2 class="text-lg font-medium text-center text-white title-font md:text-center"><i :class="icon"></i> {{ name }}</h2>
 
