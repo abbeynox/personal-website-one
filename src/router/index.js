3cf189ef6ef5e9f062574ffff7ee5feea0d60c28
Yao Kaiser <yao@ywk.ch>
2021-10-18 19:40:57 +0200

    fix: some production mode fixes

--- src/router/index.js
+++ src/router/index.js
@@ -11,10 +11,10 @@ Vue.use(VuePageTitle, {
 
 const router = new VueRouter({
   mode: 'history',
-  /*base: process.env.BASE_URL, 
+  base: process.env.BASE_URL, 
   scrollBehavior() {
     return { x: 0, y: 0 };
-  },*/
+  },
 
   routes: [
     {
