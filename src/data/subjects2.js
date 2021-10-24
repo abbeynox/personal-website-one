1f33b895a3577aff1c3d4415c0343bf3974071a1
Yao Kaiser <yao@ywk.ch>
2021-10-24 18:24:59 +0200

    update: timetable

--- src/data/subjects2.js
+++ src/data/subjects2.js
@@ -1,105 +1,114 @@
 import uniqueId from "lodash.uniqueid";
 
 const data = [
+  {
+    id: uniqueId("subject-"),
+    day: "Freitag",
+    name: "Schulfrei",
+    subName: "Wegen ITT findet die Schule heute nicht statt. Schüler:innen werden zugunsten des Betriebs freigestellt. Siehe BBW Terminplan: https://rlnk.be/oMHq.",
+    test: false
+  },
+  
+  {
+      id: uniqueId("subject-"),
+      day: "Freitag",
+      name: "Mathe",
+      startTime: "7:35",
+      endTime: "8:20",
+      room: "A313",
+      teacher: "Bukvic",
+      test: false
+    },
     {
-        id: uniqueId("subject-"),
-        day: "Freitag",
-        name: "Mathe",
-        startTime: "7:35",
-        endTime: "8:20",
-        room: "A313",
-        teacher: "Bukvic",
-        test: false
-      },
-      {
-        id: uniqueId("subject-"),
-        day: "Freitag",
-        name: "Mathe",
-        startTime: "8:25",
-        endTime: "9:10",
-        room: "A313",
-        teacher: "Bukvic",
-        test: false
-      },
-      {
-        id: uniqueId("subject-"),
-        day: "Freitag",
-        name: "Mathe",
-        startTime: "9:15",
-        endTime: "10:00",
-        room: "A313",
-        teacher: "Bukvic",
-        test: false
-      },
-      {
-        id: uniqueId("subject-"),
-        day: "Freitag",
-        name: "Englisch",
-        startTime: "10:20",
-        endTime: "11:05",
-        room: "A313",
-        teacher: "Fischer",
-        test: false
-      },
-      {
-        id: uniqueId("subject-"),
-        day: "Freitag",
-        name: "Englisch",
-        startTime: "11:10",
-        endTime: "11:55",
-        room: "A313",
-        teacher: "Fischer",
-        test: false
-      },
-      {
-        id: uniqueId("subject-"),
-        day: "Freitag",
-        name: "Mittagspause",
-        startTime: "12:00",
-        endTime: "12:45",
-        teacher: "",
-        test: false
-      },
-      {
-        id: uniqueId("subject-"),
-        day: "Freitag",
-        name: "Chemie",
-        subName: "",
-        startTime: "12:55",
-        endTime: "13:40",
-        room: "A323",
-        teacher: "Kollbrunner",
-        test: false
-      },
-      {
-        id: uniqueId("subject-"),
-        day: "Freitag",
-        name: "Chemie",
-        startTime: "13:45",
-        endTime: "14:30",
-        room: "A323",
-        teacher: "Kollbrunner",
-        test: false
-      },
-      {
-        id: uniqueId("subject-"),
-        day: "Freitag",
-        name: "Franzözisch",
-        startTime: "14:35",
-        endTime: "15:20",
-        room: "A313",
-        teacher: "Spiri",
-        test: false
-      },
-      {
-        id: uniqueId("subject-"),
-        day: "Freitag",
-        name: "Französisch",
-        startTime: "15:35",
-        endTime: "16:20",
-        room: "A313",
-        teacher: "Spiri",
-        test: false
-      }
-];
+      id: uniqueId("subject-"),
+      day: "Freitag",
+      name: "Mathe",
+      startTime: "8:25",
+      endTime: "9:10",
+      room: "A313",
+      teacher: "Bukvic",
+      test: false
+    },
+    {
+      id: uniqueId("subject-"),
+      day: "Freitag",
+      name: "Mathe",
+      startTime: "9:15",
+      endTime: "10:00",
+      room: "A313",
+      teacher: "Bukvic",
+      test: false
+    },
+    {
+      id: uniqueId("subject-"),
+      day: "Freitag",
+      name: "Englisch",
+      startTime: "10:20",
+      endTime: "11:05",
+      room: "A313",
+      teacher: "Fischer",
+      test: false
+    },
+    {
+      id: uniqueId("subject-"),
+      day: "Freitag",
+      name: "Englisch",
+      startTime: "11:10",
+      endTime: "11:55",
+      room: "A313",
+      teacher: "Fischer",
+      test: false
+    },
+    {
+      id: uniqueId("subject-"),
+      day: "Freitag",
+      name: "Mittagspause",
+      startTime: "12:00",
+      endTime: "12:45",
+      teacher: "",
+      test: false
+    },
+    {
+      id: uniqueId("subject-"),
+      day: "Freitag",
+      name: "Chemie",
+      subName: "",
+      startTime: "12:55",
+      endTime: "13:40",
+      room: "A323",
+      teacher: "Kollbrunner",
+      test: false
+    },
+    {
+      id: uniqueId("subject-"),
+      day: "Freitag",
+      name: "Chemie",
+      startTime: "13:45",
+      endTime: "14:30",
+      room: "A323",
+      teacher: "Kollbrunner",
+      test: false
+    },
+    {
+      id: uniqueId("subject-"),
+      day: "Freitag",
+      name: "Franzözisch",
+      startTime: "14:35",
+      endTime: "15:20",
+      room: "A313",
+      teacher: "Spiri",
+      test: false
+    },
+    {
+      id: uniqueId("subject-"),
+      day: "Freitag",
+      name: "Französisch",
+      startTime: "15:35",
+      endTime: "16:20",
+      room: "A313",
+      teacher: "Spiri",
+      test: false
+    }
+]; 
+
 export default data;
