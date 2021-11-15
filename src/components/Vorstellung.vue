3e6edd11123d2ac021bdbf9b8334bb371d9207b6
Yao Kaiser <yao@ywk.ch>
2021-11-15 15:21:54 +0100

    remove: about image

--- src/components/Vorstellung.vue
+++ src/components/Vorstellung.vue
@@ -15,9 +15,6 @@
         </p>
 
         </div>
-        <div class="w-5/6 lg:max-w-lg lg:w-full md:w-1/2">
-        <img class="object-cover object-center rounded" alt="hero" src="../assets/yao_holiday.jpeg">
-        </div>
     </div>
     </section>
 </template>
