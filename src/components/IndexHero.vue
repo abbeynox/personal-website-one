98b39db80e6be45dadcc5d3aa20b3b133bb04d73
Yao Kaiser <yao@ywk.ch>
2021-11-15 13:36:49 +0100

    update: header title

--- src/components/IndexHero.vue
+++ src/components/IndexHero.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container px-5 py-24 mx-auto">
         <div class="flex flex-col w-full mb-20 text-center">
-        <h1 class="mb-4 text-6xl font-bold text-white sm:text-6xl title-font">🌎yao.earth FTP</h1>
+        <h1 class="mb-4 text-6xl font-bold text-white sm:text-6xl title-font">🌎yao.earth</h1>
         <transition name="view" appear>
             <p class="mx-auto mt-2 text-lg leading-relaxed text-gray-400 lg:w-2/3" v-show="this.$route.query.redirect !== undefined && this.redirectInfo == true" id="redirectInfo">{{"➡️Weitergeleitet von " + this.$route.query.redirect + ".ch"}}</p>
         </transition>
