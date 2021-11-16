bfb0b83721aa8a482f17676865cb35697a241960
Yao Kaiser <yao@ywk.ch>
2021-11-16 09:57:09 +0100

    update: redesign + update text

--- src/views/404Page.vue
+++ src/views/404Page.vue
@@ -1,16 +1,18 @@
 <template>
-    <div class="container px-5 py-24 mx-auto">
-        <div class="flex flex-col w-full mb-20 text-center">
-            <h1 class="mb-4 text-6xl font-bold text-white sm:text-6xl title-font">404 - Diese Seite wurde nicht gefunden</h1>
-            <p class="mx-auto mt-2 text-lg leading-relaxed text-gray-400 lg:w-2/3">Du befindest dich an einem seltsamen Ort. Hast du dich eventuell vertippt? Checke die URL und versuche es nochmals.</p>
-        </div>   
+    <div>
+        <Header title="404 - Not found" description="Diese Seite wurde nicht gefunden." text="Du befindest dich an einem seltsamen Ort. Hast du dich eventuell vertippt? Überprüfe die URL und versuche es nochmals."></Header>
     </div>
 </template>
 
 
 <script>
+import Header from "../components/Header"
+
 export default {
     name: '404Page',
-    title: '404 - Nicht gefunden'
+    title: '404 - Nicht gefunden',
+    components: {
+        Header
+    }
 }
 </script>
