9ed7ab68d5bc7f7f62b672a47cda42df6991f9d0
Yao Kaiser <yao@ywk.ch>
2021-11-27 14:48:24 +0100

    add: notification component

---
+++ src/components/lib/Notification.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="relative h-auto my-5 tracking-wide bg-transparent border-2 border-gray-800 rounded-lg animate-pulse md:mx-1 md:w-2/3 justify-items-stretch mb-14 hover:bg-indigo-500 hover:bg-opacity-10 hover:shadow-lg hover:border-indigo-500">
+      <a :href="link">
+        <div class="absolute w-1 h-20 bg-purple-500 small-banner rounded-tl-md"></div>
+        
+          <div class="items-center">
+            <span class="float-left pt-3 pl-4 font-mono text-4xl text-teal-800 bg-teal-100 rounded-full md:text-5xl align-center"><i :class="icon"></i></span>
+            <div class="pt-3 pb-6 ml-16 md:ml-20">
+              <h5 class="text-2xl font-semibold text-left text-white">{{ title }}</h5>
+              <p class="text-left text-gray-400 text-md font-regular">{{ description }}</p>
+            </div>
+          </div>
+
+      </a>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Notification",
+  props: {
+    title: {
+      type: String,
+      required: true,
+      default: "N/A",
+    },
+    description: {
+      type: String,
+      required: true,
+      default: "N/A",
+    },
+    link: {
+        type: String,
+        required: false
+    },
+    icon: {
+      type: String,
+      required: true,
+      default: "fas fa-exclamation-circle"
+    }
+  },
+};
+</script>
