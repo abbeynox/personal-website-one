02e008650c91ffc8ad980f85b16f8acd64dfcb7a
Yao Kaiser <yao@ywk.ch>
2021-11-27 14:49:06 +0100

    feature: notification on links // added social media break notification

--- src/views/LinksPage.vue
+++ src/views/LinksPage.vue
@@ -1,19 +1,19 @@
 <template>
     <div>
-            <div class="container content-center px-5 pt-24 pb-1 mx-auto">
-                
-                <div class="flex flex-col content-center w-full mb-20 text-center">
-                    <img class="mx-auto w-40 h-40 text-white p-0.5 bg-purple-500 rounded-full m-5" alt="hero" src="../assets/YaoDev.jpg">
-                    <h1 class="mb-4 text-6xl font-bold text-white sm:text-6xl title-font">Yao Kaiser</h1>
-                    <p class="mx-auto mt-2 text-lg leading-relaxed text-gray-400 lg:w-2/3">Proud to be different ♠️</p>
-                </div>   
+        <div class="container content-center px-5 pt-24 pb-1 mx-auto">
+            
+            <div class="flex flex-col content-center w-full text-center md:m-1">
+                <img class="mx-auto w-40 h-40 text-white p-0.5 bg-purple-500 rounded-full m-5" alt="hero" src="../assets/YaoDev.jpg">
+                <h1 class="mb-4 text-6xl font-bold text-white sm:text-6xl title-font">Yao Kaiser</h1>
+                <Notification title="Social Media Pause" description="Während zwei Wochen auf Social Media inaktiv." link="https://yao.earth/socialmediapause"></Notification>
             </div>
+        </div>
 
-            <div class="container mx-auto">
-                <div v-for="link in links" :key="link.id" class="flex flex-wrap m-2">
-                    <LinkCard :name="link.name" :url="link.url" :icon="link.icon" :toColor="link.toColor" :fromColor="link.fromColor" :viaColor="link.viaColor"></LinkCard>
-                </div>
+        <div class="container mx-auto">
+            <div v-for="link in links" :key="link.id" class="flex flex-wrap m-2">
+                <LinkCard :name="link.name" :url="link.url" :icon="link.icon" :toColor="link.toColor" :fromColor="link.fromColor" :viaColor="link.viaColor"></LinkCard>
             </div>
+        </div>
 
     </div>
 </template>
@@ -22,6 +22,7 @@
 
     import LinkCard from '../components/cards/LinkCard';
     import links from '../data/links.js';
+    import Notification from '../components/lib/Notification';
 
     export default {
         name: 'LinksPage',
@@ -32,7 +33,8 @@
             };
         },
         components: {
-            LinkCard
+            LinkCard,
+            Notification
         },
         
         mounted: function() {
