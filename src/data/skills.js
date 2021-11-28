8b1e799f2c21098b8963455bb20546298a5e57cd
Yao Kaiser <yao@ywk.ch>
2021-11-28 21:32:15 +0100

    update: skills data

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,15 +1,15 @@
 import uniqueId from 'lodash.uniqueid';
 
-const data = [{ id: uniqueId('skill-'), name: 'Vue.js', icon: 'fab fa-vuejs', rate: 4 },
-{ id: uniqueId('skill-'), name: 'JavaScript', icon: 'fab fa-js', rate: 4 },
-{ id: uniqueId('skill-'), name: 'React.js', icon: 'fab fa-react', rate: 2 },
-{ id: uniqueId('skill-'), name: 'NPM', icon: 'fab fa-npm', rate: 4 },
+const data = [{ id: uniqueId('skill-'), name: 'Vue.js', icon: 'fab fa-vuejs', rate: 8 },
+{ id: uniqueId('skill-'), name: 'JavaScript', icon: 'fab fa-js', rate: 5 },
+{ id: uniqueId('skill-'), name: 'React.js', icon: 'fab fa-react', rate: 3 },
+{ id: uniqueId('skill-'), name: 'NPM', icon: 'fab fa-npm', rate: 6 },
 { id: uniqueId('skill-'), name: 'Java', icon: 'fab fa-java', rate: 2 },
-{ id: uniqueId('skill-'), name: 'Bootstrap', icon: 'fab fa-bootstrap', rate: 3 },
-{ id: uniqueId('skill-'), name: 'Tailwind CSS', icon: 'fas fa-paint-brush', rate: 3 },
+{ id: uniqueId('skill-'), name: 'Bootstrap', icon: 'fab fa-bootstrap', rate: 7 },
+{ id: uniqueId('skill-'), name: 'Tailwind CSS', icon: 'fas fa-paint-brush', rate: 9 },
 { id: uniqueId('skill-'), name: 'Python', icon: 'fab fa-python', rate: 4 },
 { id: uniqueId('skill-'), name: 'SQL', icon: 'fas fa-database', rate: 3 },
 { id: uniqueId('skill-'), name: 'Plesk', icon: 'fas fa-server', rate: 4 },
-{ id: uniqueId('skill-'), name: 'HTML 5', icon: 'fab fa-html5', rate: 5 }]
+{ id: uniqueId('skill-'), name: 'HTML 5', icon: 'fab fa-html5', rate: 10 }]
 export default data;
 
