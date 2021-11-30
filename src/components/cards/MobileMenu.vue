14ab7c07f088e7abdc5c579705a9889ef2fe8835
Yao Kaiser <yao@ywk.ch>
2021-11-30 15:37:44 +0100

    add: mobile menu

---
+++ src/components/cards/MobileMenu.vue
@@ -0,0 +1,163 @@
+<!-- Ursprüngliches Template: https://go.ywk.ch/twmobilemenu -->
+
+<template>
+  <div>
+    <div
+      v-show="menuOpen" 
+      class="fixed inset-x-0 bottom-0 w-full h-64 bg-white rounded-t-lg translate-y-60 md:hidden"
+      enter="duration-200 ease-out"
+      enterFrom="opacity-0 scale-95"
+      enterTo="opacity-100 scale-100"
+      leave="duration-100 ease-in"
+      leaveFrom="opacity-100 scale-100"
+      leaveTo="opacity-0 scale-95"
+    >
+      <div>
+        <div class="grid grid-cols-3 gap-4 justify-items-center">
+          <div>
+            <a
+              href="/"
+              class="flex items-end justify-center w-full px-4 pt-2 mx-auto text-center text-gray-400 hover:text-purple-500"
+            >
+              <span class="block px-1 pt-1 pb-1">
+                <i class="block pt-1 mb-1 text-2xl fas fa-home"></i>
+                <span class="block pb-2 text-xs">Home</span>
+                <span
+                  class="block w-5 h-1 mx-auto rounded-full hover:bg-purple-500"
+                ></span>
+              </span>
+            </a>
+          </div>
+          <div>
+            <a
+              href="/projects"
+              class="flex items-end justify-center w-full px-4 pt-2 mx-auto text-center text-gray-400 hover:text-purple-500"
+            >
+              <span class="block px-1 pt-1 pb-1">
+                <i class="block pt-1 mb-1 text-2xl fas fa-paint-roller"></i>
+                <span class="block pb-2 text-xs">Projekte</span>
+                <span
+                  class="block w-5 h-1 mx-auto rounded-full hover:bg-purple-500"
+                ></span>
+              </span>
+            </a>
+          </div>
+          <div>
+            <a
+              href="/about"
+              class="flex items-end justify-center w-full px-4 pt-2 mx-auto text-center text-gray-400 hover:text-purple-500"
+            >
+              <span class="block px-1 pt-1 pb-1">
+                <i class="block pt-1 mb-1 text-2xl fas fa-user-circle"></i>
+                <span class="block pb-2 text-xs">Über mich</span>
+                <span
+                  class="block w-5 h-1 mx-auto rounded-full hover:bg-purple-500"
+                ></span>
+              </span>
+            </a>
+          </div>
+          <div>
+            <a
+              href="/links"
+              class="flex items-end justify-center w-full px-4 pt-2 mx-auto text-center text-gray-400 hover:text-purple-500"
+            >
+              <span class="block px-1 pt-1 pb-1">
+                <i class="block pt-1 mb-1 text-2xl fas fa-link"></i>
+                <span class="block pb-2 text-xs">Links</span>
+                <span
+                  class="block w-5 h-1 mx-auto rounded-full hover:bg-purple-500"
+                ></span>
+              </span>
+            </a>
+          </div>
+          <div>
+            <a
+              href="/stundenplan"
+              class="flex items-end justify-center w-full px-4 pt-2 mx-auto text-center text-gray-400 hover:text-purple-500"
+            >
+              <span class="block px-1 pt-1 pb-1">
+                <i class="block pt-1 mb-1 text-2xl far fa-calendar-alt"></i>
+                <span class="block pb-2 text-xs">Stundenplan</span>
+                <span
+                  class="block w-5 h-1 mx-auto rounded-full hover:bg-purple-500"
+                ></span>
+              </span>
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://cloud.ywk.ch"
+              class="flex items-end justify-center w-full px-4 pt-2 mx-auto text-center text-gray-400 hover:text-purple-500"
+            >
+              <span class="block px-1 pt-1 pb-1">
+                <i class="block pt-1 mb-1 text-2xl fas fa-cloud"></i>
+                <span class="block pb-2 text-xs">Cloud</span>
+                <span
+                  class="block w-5 h-1 mx-auto rounded-full hover:bg-purple-500"
+                ></span>
+              </span>
+            </a>
+          </div>
+        </div>
+
+        >
+      </div>
+    </div>
+
+    <div class="fixed bottom-0 right-0 visible w-16 h-16 md:invisible">
+      <div class="relative">
+        <button
+          @click="menuOpen = !menuOpen"
+          v-click-outside="onClickOutside"
+          class="text-gray-400 transition duration-100 ease-in-out fill-current hover:text-purple-500"
+        >
+          <svg
+            :class="(menuOpen)?'duration-300 transform rotate-45':' duration-300 transform rotate-0'"
+            xmlns="http://www.w3.org/2000/svg"
+            width="40"
+            height="40"
+            viewBox="0 0 24 24"
+          >
+            <path
+              d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm5 11h-4v4h-2v-4H7v-2h4V7h2v4h4v2z"
+            ></path>
+          </svg>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+
+Vue.directive("click-outside", {
+  bind(el, binding, vnode) {
+    el.clickOutsideEvent = (event) => {
+      if (!(el === event.target || el.contains(event.target))) {
+        vnode.context[binding.expression](event);
+      }
+    };
+    document.body.addEventListener("click", el.clickOutsideEvent);
+  },
+  unbind(el) {
+    document.body.removeEventListener("click", el.clickOutsideEvent);
+  },
+});
+
+export default {
+  name: "mobile menu",
+  data() {
+    return {
+      menuOpen: false,
+    };
+  },
+  methods: {
+    onClickOutside() {
+      this.menuOpen = false;
+
+    },
+  },
+};
+
+</script>
