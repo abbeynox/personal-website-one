189f11a1acdd8b1066fbaf06f67a6780d0350a20
Yao Kaiser <yao@ywk.ch>
2021-11-30 09:19:03 +0100

    remove: Twitter API req

--- src/components/SocialMedia.vue
+++ src/components/SocialMedia.vue
@@ -12,26 +12,6 @@ export default {
   components: {
     Header,
   },
-  mounted() {
-    var axios = require('axios');
-    var AUTH_OBJ = {
-      oauth_consumer_key: "NOAEuJ0g8emWaEwycuFwI1wZH", // API Key
-      oath_consumer_secret: "PEyo4elLKvWtNZoFWcfju5qXWWzrAl9sL7m5VRh1JQV7Ccjf1Z", //API Secret Key  
-      oauth_signature_method: "HMAC-SHA1",
-      oauth_timestamp: "1473647719",
-      oauth_token: "PEyo4elLKvWtNZoFWcfju5qXWWzrAl9sL7m5VRh1JQV7Ccjf1Z",
-    };
-
-    axios.defaults.headers.common['Authorization'] = AUTH_OBJ;
-
-    axios.get('https://api.twitter.com/1.1/trends/place.json', {
-      id: 1
-    }).then(function(resp) {
-      console.log(resp);
-    }).catch(function(e) {
-      console.log(e)
-    });
-  },
   data() {
     return {
     }
