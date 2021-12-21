cd0c1a78979afd9ed2b2ab96b047baf01460d93e
Yao Kaiser <yao@ywk.ch>
2021-12-21 09:25:49 +0100

    update data

--- src/data/subjects.js
+++ src/data/subjects.js
@@ -10,7 +10,7 @@ const data = [
         endTime: "8:20",
         room: "3.3",
         teacher: "Cavouti",
-        test: true
+        test: false
       },
       {
         id: uniqueId("subject-"),
